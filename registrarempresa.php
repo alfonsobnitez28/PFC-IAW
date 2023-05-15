5f9609d9747b44c445d2d075a254846c47a98632
Alfonso <abengar577a@ieslacampina.es>
2023-05-15 17:29:04 +0200

    añado imagen de titulo con el logo del insituto

--- registrarempresa.php
+++ registrarempresa.php
@@ -20,6 +20,7 @@ echo $fila ['id_alumno'];
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="descarga.png">
     <title>Gestión de alumnos IES La Campiña</title>
 </head>
 <body>
